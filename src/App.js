ef8447689e2bb2dd57e811bb829aa8b3395f8f9a
yogeshdas65 <yogeshdas65@gmail.com>
2024-11-02 12:10:47 +0530

    adding slice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout';
+import { Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import ProductDetails from './pages/ProductDetails';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path="/products/:pid" element={<ProductDetails />} />
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="mt-4 bg-dark text-white">
+            <div className="container p-4 text-center">
+                <h2>Design And Developed By: Yogesh Das</h2>
+                <h6>All Right Reserved by &copy; Yogesh Das</h6>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
